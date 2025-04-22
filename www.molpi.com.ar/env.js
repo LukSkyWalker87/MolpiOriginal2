ef13add6f4fd680e45b4dfd373879c8dd2d46ea2
LukSkyWalker87 <84882795+LukSkyWalker87@users.noreply.github.com>
2025-04-22 13:51:17 -0300

    v5.1

--- www.molpi.com.ar/env.js
+++ www.molpi.com.ar/env.js
@@ -1,2 +1,4 @@
 // Este archivo debe estar en la raíz del proyecto Netlify
-window.API_URL = 'https://molpisoloback-production.up.railway.app';
+window.env = {
+    API_URL: 'https://molpisoloback-production.up.railway.app'
+};
